f79c1329be851a187e7eabff64047259201ce6d9
mlisboabreu <164585526+mlisboabreu@users.noreply.github.com>
2025-04-16 19:40:30 -0300

    Update sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -3,18 +3,12 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-    // Verifica se o número de argumentos é válido
+    
     if (argc != 2) {
         fprintf(2, "Usage: sleep <ticks>\n");
         exit(1);
     }
-
-    // transforma em inteiro
     int ticks = atoi(argv[1]);
-
-    //syscall sleep
     sleep(ticks);
-
-    // Finaliza o programa
     exit(0);
 }
